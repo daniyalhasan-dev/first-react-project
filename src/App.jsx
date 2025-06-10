869e680c5b33915894366393a1936b90ff98cfce
Daniyal Hasan <daniyalhasan223@gmail.com>
2025-06-10 16:18:22 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import React from 'react'
+import './App.css'
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
+import Hero from './components/hero/hero'
+import Pricing from './components/pricing/pricing'
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <div className='min-h-screen '>
+    <Header/>
+    <Hero/>
+    <Pricing/>
+    <Footer/>
+    </div>
+    </React.Fragment>
+  )
+  
+}
+
+export default App
